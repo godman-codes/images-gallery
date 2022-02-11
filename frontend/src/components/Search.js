7e1ab7e6d53faae75a4c22178a7656b161f72279
Faseun <faseunoluwatobilobasimon@gmail.com>
2022-02-11 17:55:16 +0000

    adjust vs code tab size

--- frontend/src/components/Search.js
+++ frontend/src/components/Search.js
@@ -1,32 +1,32 @@
-import React from "react";
-import { Container, Row, Col, Button, Form } from "react-bootstrap";
+import React from 'react';
+import { Container, Row, Col, Button, Form } from 'react-bootstrap';
 
 const Search = ({ word, setWord, handleSubmit }) => {
-   return (
-      <Container className="mt-4">
-         <Row className="justify-content-center">
-            <Col xs={12} md={8} lg={6}>
-               <Form onSubmit={handleSubmit}>
-                  <Form.Row>
-                     <Col xs={9}>
-                        <Form.Control
-                           text="text"
-                           value={word}
-                           onChange={(e) => setWord(e.target.value)}
-                           placeholder="Seach for new image"
-                        />
-                     </Col>
-                     <Col>
-                        <Button variant="primary" type="submit">
-                           Search
-                        </Button>
-                     </Col>
-                  </Form.Row>
-               </Form>
-            </Col>
-         </Row>
-      </Container>
-   );
+  return (
+    <Container className="mt-4">
+      <Row className="justify-content-center">
+        <Col xs={12} md={8} lg={6}>
+          <Form onSubmit={handleSubmit}>
+            <Form.Row>
+              <Col xs={9}>
+                <Form.Control
+                  text="text"
+                  value={word}
+                  onChange={(e) => setWord(e.target.value)}
+                  placeholder="Seach for new image"
+                />
+              </Col>
+              <Col>
+                <Button variant="primary" type="submit">
+                  Search
+                </Button>
+              </Col>
+            </Form.Row>
+          </Form>
+        </Col>
+      </Row>
+    </Container>
+  );
 };
 
 export default Search;
